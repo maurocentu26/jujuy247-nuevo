68cf78003ff3eed3e053596cf9c43d35d16fb15e
maurocenturion26 <maurocenturion26@gmail.com>
2026-04-12 19:50:48 -0300

    Update SEO share image on X(Twitter)/Sharelink

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -7,6 +7,26 @@ export const metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'),
   title: 'Jujuy247',
   description: 'Portal de noticias',
+  openGraph: {
+    title: 'Jujuy247',
+    description: 'MANTENETE INFORMADO CON JUJUY24/7, TU PORTAL DE NOTICIAS EN JUJUY. ÚLTIMAS NOTICIAS, ANÁLISIS Y MÁS.',
+    siteName: 'Jujuy247',
+    type: 'website',
+    images: [
+      {
+        url: '/seo-imgs/share-img.jpeg',
+        width: 1200,
+        height: 630,
+        alt: 'Jujuy247',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Jujuy247',
+    description: 'MANTENETE INFORMADO CON JUJUY24/7, TU PORTAL DE NOTICIAS EN JUJUY. ÚLTIMAS NOTICIAS, ANÁLISIS Y MÁS.',
+    images: ['/seo-imgs/share-img.jpeg'],
+  },
 };
 
 export default function RootLayout({ children }) {
